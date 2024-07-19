6f2817ecf4a8c057fa75be9db1da9680ba0ea8f9
jccartwright <john.c.cartwright@comcast.net>
2024-07-19 15:17:20 -0600

    construct table name from env variables

--- fy_counts_input/app.py
+++ fy_counts_input/app.py
@@ -5,6 +5,8 @@ import os
 
 logger = logging.getLogger()
 logger.setLevel(os.environ.get("LOGLEVEL", "WARNING"))
+ATHENA_DATABASE = os.environ.get("ATHENA_DATABASE", "dcdb")
+ATHENA_TABLE = os.environ.get("ATHENA_TABLE", "csb_parquet")
 
 
 def lambda_handler(event, context):
@@ -14,10 +16,14 @@ def lambda_handler(event, context):
     response = []
 
     for yr in range(start_year, current_year):
+        # HACK until a better way to pass value into ASL Map iteration
         response.append({
             'name': f'FY{str(yr)[2:]}',
             'start': f'{yr - 1}-10-01',
-            'end': f'{yr}-09-30'
+            'end': f'{yr}-09-30',
+            'table': f'{ATHENA_DATABASE}.{ATHENA_TABLE}'
         })
 
-    return {'years': response}
+    return {
+        'years': response
+    }
